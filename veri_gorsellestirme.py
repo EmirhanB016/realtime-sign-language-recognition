bcc664b2a6598946de033da3df18277d65b00b82
EmirhanB016 <beyogluemirhan6@gmail.com>
2026-06-02 13:49:45 +0300

    Gereksiz kodlar kaldırıldı.

--- veri_gorsellestirme.py
+++ veri_gorsellestirme.py
@@ -4,17 +4,13 @@ import matplotlib.pyplot as plt
 
 print("Veri seti yükleniyor lütfen bekleyin...")
 
-# Veriyi "data" klasörünün içinden okuyoruz
 veri = pd.read_csv("data/sign_mnist_train.csv")
 
-# İlk satırdaki veriyi alıyoruz
 ilk_satir = veri.iloc[0]
 
-# İlk sütun harfin ne olduğu (etiket), geri klan 784 sütun piksel değerleridir
 etiket = ilk_satir["label"]
 pikseller = ilk_satir[1:].values
 
-# 784 uzunluğundaki düz sayı listesini 28x28 boyutunda bir resim matrisine çeviriyoruz
 resim = pikseller.reshape(28, 28).astype(float)
 
 plt.imshow(resim, cmap = "gray")
